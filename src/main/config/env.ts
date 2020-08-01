b2d7b2b1d32c67adcb5be7e7e208fa795dcd0885
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-08-01 13:14:23 -0300

    Delete env.ts

--- src/main/config/env.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
-  port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'mfs4=CMPC/g'
-}
